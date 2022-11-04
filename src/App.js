168966f19ac95aa17467a685cf15b287807585c5
Mike Rugh <mike.rugh@gmail.com>
2022-11-04 16:41:19 -0700

    adds components

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import About from './components/About';
+
+function App() {
+  return (
+    <div className="reactafolio-app">
+      <About />
+    </div>
+  );
+}
+
+export default App;
