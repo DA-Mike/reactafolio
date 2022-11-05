b7b8f6682cd327fae3450738334bc1974ac84062
Mike Rugh <mike.rugh@gmail.com>
2022-11-04 20:49:14 -0700

    adds contact and portfolio

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,1 +1,13 @@
 import React from 'react';
+
+function Contact() {
+    return (
+        <div>
+            <h1>Contact Me</h1>
+            <div>Placeholder</div>
+            <div>Test Test Test</div>
+        </div>
+    );
+};
+
+export default Contact;
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,1 +1,13 @@
 import React from 'react';
+
+function Portfolio() {
+    return (
+        <div>
+            <h1>My Portfolio</h1>
+            <div>Placeholder</div>
+            <div>Test Test Test</div>
+        </div>
+    );
+};
+
+export default Portfolio;
