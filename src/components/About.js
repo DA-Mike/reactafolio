a87811aa392fa92a0f9c199dd0cbafb5768c7784
Mike Rugh <mike.rugh@gmail.com>
2022-11-04 20:28:13 -0700

    adds to About

--- src/components/About.js
+++ src/components/About.js
@@ -1,2 +1,13 @@
 import React, { useState } from 'react';
 
+function About() {
+    return (
+        <div>
+            <h1>About Me</h1>
+            <div>Image Placeholder</div>
+            <div>Test Test Test</div>
+        </div>
+    );
+};
+
+export default About;
