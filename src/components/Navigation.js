63b09ede1c7c91982a5124ddcc4f6fa71f67339c
Mike Rugh <mike.rugh@gmail.com>
2022-11-07 12:15:07 -0800

    adds responsive navbar and some files

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,17 +2,29 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-// import bootstrap from 'bootstrap';
-// import { Collapse } from bootstrap;
-// import Collapse from 'react-bootstrap/Collapse'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 
 function Navigation({ currentPage, handlePageChange }) {
+  const hide = {
+    '@media (maxWidth: 768px)': {
+      display: 'none',
+    },
+  };
+
+  const show = {
+    '@media (maxWidth: 768px)': {
+      display: 'block',
+      margin: 0,
+      padding: 0,
+      color: 'white',
+    },
+  };
+
   return (
     <>
-      <Navbar bg="primary" variant="dark" class='navbar'>
-        <Container>
+      <Navbar bg="primary" variant="dark" className="nav-items" style={hide} id="nav-full">
+        <Container className="nav-items">
           <Navbar.Brand href="#about" onClick={() => handlePageChange('About')}
           className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}>Mike Rugh</Navbar.Brand>
           <Nav className="nav-items">
@@ -25,10 +37,14 @@ function Navigation({ currentPage, handlePageChange }) {
             <Nav.Link href="#resume" onClick={() => handlePageChange('Resume')}
             className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}>Resume</Nav.Link>
           </Nav>
-          <div className="container-fluid nav-ham">
-          <nav className="navbar navbar-expand-lg navbar-light bg-light m-4 rounded shadow">
+        </Container>
+      </Navbar>
+          <div className="container nav-ham" style={show}>
+          <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
               <div className="container-fluid">
-                  <button className="navbar-toggler" type="button"
+                <Navbar.Brand href="#about" onClick={() => handlePageChange('About')}
+                className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}>Mike Rugh</Navbar.Brand>
+                  <button className="navbar-toggler collapsed" type="button"
                       data-bs-toggle="collapse" data-bs-target="#navbarNav"
                       aria-controls="navbarNav" aria-expanded="false"
                       aria-label="Toggle navigation">
@@ -37,23 +53,26 @@ function Navigation({ currentPage, handlePageChange }) {
                   <div className="collapse navbar-collapse" id="navbarNav">
                       <ul className="navbar-nav ms-auto">
                           <li className="nav-item">
-                              <a aria-current="page" href="#about"  onClick={() => handlePageChange('About')}
-                                  className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}>About</a>
+                              <a href="#about" onClick={() => handlePageChange('About')}
+                              className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}>About Me</a>
                           </li>
                           <li className="nav-item">
-                              <a className="nav-link" href="#portfolio">Portfolio</a>
+                              <a href="#portfolio"onClick={() => handlePageChange('Portfolio')}
+                              className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}>Portfolio</a>
                           </li>
                           <li className="nav-item">
-                              <a className="nav-link" href="/">Contact</a>
+                              <a href="#contact"onClick={() => handlePageChange('Contact')}
+                              className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}>Contact</a>
+                          </li>
+                          <li className="nav-item">
+                              <a href="#resume"onClick={() => handlePageChange('Resume')}
+                              className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}>Resume</a>
                           </li>
                       </ul>
                   </div>
               </div>
           </nav>
-          </div>
-        </Container>
-      </Navbar>
-      
+          </div>      
     </>
   );
 }
--- src/components/Project.js
+++ src/components/Project.js
@@ -7,7 +7,7 @@ export default function Project({ title, description, depUrl, gitHub }) {
     return (
         <div>
             <div className='card project-card' bg='primary'>
-                <a href={depUrl}><h4>{title}</h4></a><p><a href={gitHub}><div><BsGithub /></div></a></p>
+                <a href={depUrl}><h4>{title}</h4></a><p><a href={gitHub}><BsGithub /></a></p>
                 <h6>{description}</h6>
             </div>
         </div>
