071c81adde12fead13df2f62bc6964a2c1f6bb6f
Mike Rugh <mike.rugh@gmail.com>
2022-12-10 18:08:00 -0800

    improves responsiveness

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,6 +4,7 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
+import './styles/pages.css';
 
 function Navigation({ currentPage, handlePageChange }) {
   const hide = {
--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import '../styles/pages.css';
-import { CgProfile } from 'react-icons/cg';
+import profilePic from '../../assets/images/profile_pic.jpg'
 
 function About() {
     return (
         <div>
             <div className='page-container'>
                 <h1 className='about gradient-text'>About Me</h1>
-                <div className='aboutIcon'><CgProfile /></div>
+                <div className='aboutPic'><img src={profilePic} alt='profile' className='profile-pic'></img></div>
                 <div className='aboutText'>Hello! My name is Mike Rugh, I'm a full stack engineer currently seeking employment opportunities. My background is in SDLC management, and having taken many applications and major enhancements from conceptualization to deployment I believe I can add value to almost any team.</div>
             </div>
         </div>
